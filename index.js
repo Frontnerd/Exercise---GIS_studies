bd7f50819e75df201306658338067dc535863e71
Frontnerd <info@frontnerd.com>
2016-07-01 08:41:25 +0200

    separate geojson for github visualization

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var map = L.map('map').setView([39.75621, -104.99404], 8);
 
 
 L.tileLayer('https://api.mapbox.com/styles/v1/anselmo21/cipzlyluk0019d2kmopw6aom7/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'Dark',
-    accessToken: 'pk.eyJ1IjoiYW5zZWxtbzIxIiwiYSI6ImNpcHpsdnVweDAwYjlpNG0ycjU0ZnVidzEifQ.V7K2vNq83jiqf6oABlyKfA'
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'Dark',
+  accessToken: 'pk.eyJ1IjoiYW5zZWxtbzIxIiwiYSI6ImNpcHpsdnVweDAwYjlpNG0ycjU0ZnVidzEifQ.V7K2vNq83jiqf6oABlyKfA'
 }).addTo(map);
 
 // debugging the position
@@ -16,30 +16,22 @@ function onMapClick(e) {
 map.on('click', onMapClick);
 
 
-// geojson
-var geojsonFeature = {
-    "type": "Feature",
-    "properties": {
-        "name": "Coors Field",
-        "amenity": "Baseball Stadium",
-        "popupContent": "This is where the Rockies play!"
-    },
-    "geometry": {
-        "type": "Point",
-        "coordinates": [-104.99404, 39.75621]
-    }
-};
+// Geojson data
 
-//L.geoJson(geojsonFeature).addTo(map);
-
-
-var myLines = [{
-    "type": "LineString",
-    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
-}, {
-    "type": "LineString",
-    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
-}];
-
+// 
 var myLayer = L.geoJson().addTo(map);
 myLayer.addData(geojsonFeature);
+myLayer.addData(myLines);
+// Adding Style
+L.geoJson(myLines, {
+    style: myStyle
+}).addTo(map);
+
+
+
+
+
+
+
+
+
